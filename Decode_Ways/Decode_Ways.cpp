72a6e5e11a2de34fa1db8fb693cee7fcbee98041
kennethhu <dragon418@126.com>
2014-05-20 21:44:15 +0800

    Decode Ways - version 1

---
+++ Decode_Ways/Decode_Ways.cpp
@@ -0,0 +1,69 @@
+// Decode_Ways.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+
+        if (n == 0) return n;
+
+        int *countArr = new int[n + 1];
+        countArr[n] = countArr[n - 1] = 1;
+        if (s[n - 1] == '0')
+        {
+            delete[] countArr;
+            return 0;
+        }
+
+        for (int pos = n - 2; pos >= 0; pos--)
+        {
+            if (s[pos] == '0')
+            {
+                delete[] countArr;
+                return 0;
+            }
+
+            countArr[pos] = countArr[pos + 1];
+            if (strToNum(s, pos, 2) <= 26)
+                countArr[pos] += countArr[pos + 2];
+        }
+
+        int retVal = countArr[0];
+        delete[] countArr;
+        return retVal;
+    }
+
+private:
+    int strToNum(const string& s, int start, int characterCount)
+    {
+        int     num = 0;
+        while (characterCount > 0)
+        {
+            num = num * 10 + s[start] - '0';
+            start++;
+            characterCount--;
+        }
+
+        return num;
+    }
+
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    string s = "0";
+
+    Solution so;
+    cout<<so.numDecodings(s)<<endl;
+
+    system("PAUSE");
+	return 0;
+}
+
