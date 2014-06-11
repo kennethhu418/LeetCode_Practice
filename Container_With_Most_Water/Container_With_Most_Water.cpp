93e6d9c15d57cd5b5533d5857a82044b7286a4c4
kennethhu418 <dragon418@126.com>
2014-06-11 14:13:55 +0800

    Container with Most Water - second version

--- Container_With_Most_Water/Container_With_Most_Water.cpp
+++ Container_With_Most_Water/Container_With_Most_Water.cpp
@@ -4,77 +4,45 @@
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using std::vector;
 
 class Solution {
 public:
-    Solution()    {       top = 0;    }
-
     int maxArea(vector<int> &height) {
         int n = height.size();
 
-        if (n < 2)  return 0;
+        if (n < 2)
+            return 0;
 
-        int maxWater = -1, curHeight = 0, curCount = 0;
-        heightStack.push_back(height[0]);
-        countStack.push_back(0);
-        top = 1;
+        int maxAreaVal = 0;
 
-        for (int i = 1; i < n; i++)
+        int start = 0, end = n - 1, curArea;
+
+        while (start < end)
         {
-            if (height[i] < heightStack[top - 1])
+            if (height[start] < height[end])
             {
-                heightStack.push_back(height[i]);
-                countStack.push_back(1);
-                top++;
-                if (height[i] > maxWater)
-                    maxWater = height[i];
-                continue;
+                curArea = (end - start)*height[start];
+                start++;
             }
-
-            curHeight = std::min(height[i], heightStack[0]);
-            curCount = 1;
-            while (top > 0 && heightStack[top - 1] < height[i])
-            {
-                curCount += countStack[top - 1];
-                heightStack.pop_back();
-                countStack.pop_back();
-                top--;
-            }
-
-            heightStack.push_back(height[i]);
-            if (top > 0)
-                countStack.push_back(curCount);
             else
-                countStack.push_back(0);
-            top++;
+            {
+                curArea = (end - start)*height[end];
+                end--;
+            }
 
-            if (curCount*curHeight > maxWater)
-                maxWater = curCount*curHeight;
+            if (curArea > maxAreaVal)
+                maxAreaVal = curArea;            
         }
 
-        Reset();
-        return maxWater;
-    }
-
-private:
-    vector<int> heightStack;
-    vector<int> countStack;
-    int top;
-
-    inline void Reset()
-    {
-        heightStack.clear();
-        countStack.clear();
-        top = 0;
+        return maxAreaVal;
     }
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    int heightArr[] = {2, 3, 2, 4, 8, 3};
+    int heightArr[] = {1, 2, 1};
     vector<int> heights(heightArr, heightArr + sizeof(heightArr)/sizeof(int));
 
     Solution so;
