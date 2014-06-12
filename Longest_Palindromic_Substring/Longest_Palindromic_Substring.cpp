9c4f111947b80e088f8592520122ccdfa81f42b8
kennethhu418 <dragon418@126.com>
2014-06-12 11:37:12 +0800

    Longest Parlindromic Substring - second version

--- Longest_Palindromic_Substring/Longest_Palindromic_Substring.cpp
+++ Longest_Palindromic_Substring/Longest_Palindromic_Substring.cpp
@@ -5,69 +5,121 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <vector>
 
+using std::vector;
 using std::string;
 
+#define size_t  std::size_t
+#define INVALID_CHAR  '@'
+
+//第一版本的算法的时间复杂度是O(n^2)且空间复杂度也是O(n^2)
+//这里我们采用时间复杂度和空间复杂度均为O(n)的解法
+//此算法称为 Manacher’s algorithm，whose detail description can be found @
+//http://leetcode.com/2011/11/longest-palindromic-substring-part-ii.html
+//,which is also attached in the project
 class Solution {
 public:
     string longestPalindrome(const string &s) {
-        int n = s.size();
-        if (n < 2)  return s;
+        size_t n = s.size();
+        if (n < 2) return s;
 
-        AllocateMemory(n);
+        convertString(s);
 
-        int len = 1;
-        int maxLen = 1, maxStart = 0, maxEnd = 0;
+        size_t  center = 0, rightBounder = 0, mirrorPos = 0;
+        radiusArray[0] = 0;
 
-        while (len <= n)
+        for (size_t i = 1; i < TSize; i++)
         {
-            for (int curPos = 0; curPos <= n - len; curPos++)
+            radiusArray[i] = 0;
+
+            if (i <= rightBounder)
             {
-                if (s[curPos] != s[curPos + len - 1])
+                mirrorPos = 2 * center - i;
+
+                if (i + radiusArray[mirrorPos] < rightBounder) //PAY ATTENTION: CANNOT BE <=
+                {
+                    radiusArray[i] = radiusArray[mirrorPos];
+                    continue;
+                }
+
+                radiusArray[i] = rightBounder - i;
+                extendRadius(i, rightBounder + 1);
+
+                if (i + radiusArray[i] <= rightBounder)
                     continue;
 
-                if (len < 3)
-                    palArray[curPos][curPos + len - 1] = true;
-                else if(palArray[curPos + 1][curPos + len - 2])
-                    palArray[curPos][curPos + len - 1] = true;
-
-                if (palArray[curPos][curPos + len - 1] == true && len > maxLen)
-                {
-                    maxLen = len;  maxStart = curPos; maxEnd = curPos + len - 1;
-                }
+                center = i;
+                rightBounder = i + radiusArray[i];
             }
-
-            len++;
+            else
+            {
+                center = i;
+                extendRadius(i, i + 1);
+                rightBounder = i + radiusArray[i];
+            }            
         }
 
-        DestroyMemory();
-        return s.substr(maxStart, maxLen);
+        center = findMaxValPos();
+        int startPos = (center - radiusArray[center])/2;
+        int size = radiusArray[center];
+
+        radiusArray.clear();
+        T.clear();
+
+        return s.substr(startPos, size);
     }
 
 private:
-    bool**  palArray;
-    char*   buffer;
+    string     T;
+    size_t      TSize;
+    vector<size_t>  radiusArray;
 
-    void AllocateMemory(int n)
+    void convertString(const string& s)
     {
-        if (n == 0) return;
+        T = INVALID_CHAR;
+        size_t n = s.size();
 
-        int lineSize = n*sizeof(bool);
-        buffer = new char[n*lineSize + n*sizeof(bool*)];
-        assert(buffer);
+        for (size_t i = 0; i < n; i++)
+        {
+            T += s[i];
+            T += INVALID_CHAR;
+        }
 
-        memset(buffer, 0, n*lineSize + n*sizeof(bool*));
-
-        palArray = (bool**)(buffer + n*lineSize);
-        for (int i = 0; i < n; i++)
-            palArray[i] = (bool*)(buffer + i*lineSize);
+        TSize = T.size();
+        radiusArray.resize(TSize);
     }
 
-    void DestroyMemory()
+    inline void extendRadius(size_t center, size_t startPos)
     {
-        delete[] buffer;
+        size_t mirror = 0;
+        while (2 * center >= startPos && startPos < TSize)
+        {
+            mirror = 2 * center - startPos;
+            if (T[mirror] == T[startPos])
+            {
+                radiusArray[center]++;
+                startPos++;
+            }
+            else
+                break;
+        }
     }
 
+    size_t findMaxValPos()
+    {
+        int maxVal = radiusArray[0], maxValPos = 0;
+        for (size_t i = 1; i < TSize; i++)
+        {
+            if (maxVal < radiusArray[i])
+            {
+                maxVal = radiusArray[i];
+                maxValPos = i;
+            }
+        }
+
+        return maxValPos;
+    }
 };
 
 int _tmain(int argc, _TCHAR* argv[])
