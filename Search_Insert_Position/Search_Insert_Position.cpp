8f33f4b5f7527a9ed3e0832aba9243773d60ba86
kennethhu <dragon418@126.com>
2014-10-05 18:00:18 +0800

    Update

--- Search_Insert_Position/Search_Insert_Position.cpp
+++ Search_Insert_Position/Search_Insert_Position.cpp
@@ -1,46 +1,24 @@
 // Search_Insert_Position.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
-#include <assert.h>
-
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
         if (n == 0) return 0;
-
-        if (target < A[0])   return 0;
-        if (target > A[n - 1]) return n;
-
         int start = 0, end = n - 1, mid = 0;
-        while (start < end)
-        {
-            mid = (start + end) / 2;
-
-            if (target == A[mid])   return mid;
-            if (target > A[mid])
-            {
-                start = mid + 1;
-                continue;
-            }
-
-            end = mid - 1;
+        while (start <= end){
+            mid = ((start + end) >> 1);
+            if (target == A[mid]) return mid;
+            else if (target > A[mid]) start = mid + 1;
+            else end = mid - 1;
         }
-
-        if (start == end)
-        {
-            if (A[start] == target || A[start] > target)
-                return start;
-            return start + 1;
-        }
-
-        //start > end
-        return start;
+        if (start > end || target < A[start]) return start;
+        return start + 1;
     }
 };
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
 	return 0;
 }
--- Search_for_a_Range/Search_for_a_Range.cpp
+++ Search_for_a_Range/Search_for_a_Range.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include <assert.h>
 #include <vector>
 
 using std::vector;
@@ -10,93 +9,35 @@ using std::vector;
 class Solution {
 public:
     vector<int> searchRange(int A[], int n, int target) {
-        vector<int> result(2, -1);
+        vector<int> resultArr(2);
+        resultArr[0] = resultArr[1] = -1;
 
-        if (n == 0) return result;
+        int startIndex = searchEdge(A, n, target);
+        if (startIndex == -1) return resultArr;
 
-        array = A; size = n;
-        
-        int start = findStart(target);
-        if (start == -1)
-            return result;
-
-        int end;
-        //trim to end the function ASAP
-        if ((start == 0 || A[start - 1] != target) && (start == n - 1 || A[start + 1] != target))
-            end = start;
-        else
-        {
-            end = findEnd(target);
-            assert(end >= 0);
-        }
-
-        result[0] = start; result[1] = end;
-        return result;
+        int endIndex = searchEdge(A, n, target, false);
+        resultArr[0] = startIndex; resultArr[1] = endIndex;
+        return resultArr;
     }
 
 private:
-    int*    array;
-    int      size;
-
-    inline int findStart(int target)
-    {
-        int start = 0, end = size - 1, mid = -1;
-        while (start < end)
-        {
-            mid = (start + end) / 2;
-            if (target < array[mid])
-            {
-                end = mid - 1;  continue;
+    int searchEdge(int A[], int n, int target, bool searchLeftEdge = true){
+        int start = 0, end = n - 1, mid = 0;
+        while (start <= end){
+            mid = ((start + end) >> 1);
+            if (target == A[mid]){
+                if (searchLeftEdge){
+                    if (mid - 1 < start || A[mid - 1] != target) return mid;
+                    end = mid - 1;
+                }
+                else {
+                    if (mid + 1 > end || A[mid + 1] != target) return mid;
+                    start = mid + 1;
+                }
             }
-            if (target > array[mid])
-            {
-                start = mid + 1; continue;
-            }
-
-            if (mid == start || array[mid - 1] != target)
-                return mid;
-
-            end = mid - 1;
+            else if (target > A[mid]) start = mid + 1;
+            else end = mid - 1;
         }
-
-        if (start == end)
-        {
-            if (array[start] == target)
-                return start;
-            return -1;
-        }
-
-        return -1;
-    }
-
-    inline int findEnd(int target)
-    {
-        int start = 0, end = size - 1, mid = -1;
-        while (start < end)
-        {
-            mid = (start + end) / 2;
-            if (target < array[mid])
-            {
-                end = mid - 1;  continue;
-            }
-            if (target > array[mid])
-            {
-                start = mid + 1; continue;
-            }
-
-            if (array[mid + 1] != target)
-                return mid;
-
-            start = mid + 1;
-        }
-
-        if (start == end)
-        {
-            if (array[start] == target)
-                return start;
-            return -1;
-        }
-
         return -1;
     }
 };
